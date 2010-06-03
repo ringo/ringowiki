4bc31b1002d5c33664ec2aba5b84e5cb756bb43c
Hannes Wallnöfer <hannesw@gmail.com>
2010-06-03 22:07:21 +0200

    Add PageIndex singleton to map page names to page ids

--- model.js
+++ model.js
@@ -1,14 +1,42 @@
 require('core/array');
-export('Page');
+export('Page', 'PageIndex');
 
-var Page = require('./config').store.defineEntity('Page');
+var store = require('./config').store;
+// PageIndex is a singleton object that maps page names to page ids
+var PageIndex = store.defineEntity('PageIndex');
+var Page = store.defineEntity('Page');
+
+// create PageIndex singleton
+var index = PageIndex.all()[0];
+if (!index) {
+    index = new PageIndex();
+    index.save();
+}
+
+PageIndex.prototype.updatePage = sync(function(oldName, newName, id) {
+    if (oldName) {
+        delete this[escapeName(oldName)];
+    }
+    if (newName && id != null) {
+        this[escapeName(newName)] = id;
+    }
+    this.save();
+}, store);
 
 Page.byName = function(name) {
-    name = name.toLowerCase().replace(/\s/g, '_');
-    var pages = Page.all().filter(function(page) {
-        return name == page.name.toLowerCase().replace(/\s/g, '_');
-    });
-    return pages[0];
+    name = escapeName(name);
+    var pageId = index[name];
+    var page = pageId != null && Page.get(pageId);
+    if (!page) {
+        var pages = Page.all().filter(function(page) {
+            return name == page.name.toLowerCase().replace(/\s/g, '_');
+        });
+        page = pages[0];
+        if (page) {
+            index.updatePage(null, page.name, page._id);
+        }
+    }
+    return page;
 };
 
 Page.prototype.addRevision = function(body, created) {
@@ -24,6 +52,13 @@ Page.prototype.getRevision = function(version) {
 };
 
 Page.prototype.updateFrom = function(obj) {
-    this.name = obj.name;
+    if (this.name != obj.name) {
+        index.updatePage(this.name, obj.name, this._id);
+        this.name = obj.name;
+    }
     this.addRevision(obj.body, new Date());
 };
+
+function escapeName(name) {
+    return name ? name.toLowerCase().replace(/\s/g, '_') : null;
+}
