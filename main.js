923cda8f11e4a80feacd5a430195e74984746f80
Simon Oberhammer <simon@nekapuzer.at>
2012-08-30 11:38:16 +0200

    also make static directories configurable

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 #!/usr/bin/env ringo
 
-exports.config = module.singleton('wikiconfig', function() {
+var config = exports.config = module.singleton('wikiconfig', function() {
    if (system.args[1]) {
       var fs = require('fs');
       return {
          templates: [module.resolve('./templates/'), fs.resolve(system.args[1], './templates/')],
-         db: fs.resolve(system.args[1], './db/')
+         db: fs.resolve(system.args[1], './db/'),
+         publics: [module.resolve('public'), fs.resolve(system.args[1], './public/')]
       }
    }
    return {
       templates: [module.resolve('./templates/')],
-      db: module.resolve('./db/')
+      db: module.resolve('./db/'),
+      publics: [module.resovle('public')]
    }
 });
 
 var app = exports.app = require("./actions").app;
 app.configure("error", "notfound", "static");
-app.static(module.resolve("public"));
+config.publics.forEach(function(pub) {
+   app.static(pub);
+});
+
 
 // main script to start application
 if (require.main == module) {
